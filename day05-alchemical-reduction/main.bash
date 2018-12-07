ffe13d38ff6ee0f677a7a2b9744b8b9524acc1be
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-07 19:38:36 +0100

    testing travis

--- day05-alchemical-reduction/main.bash
+++ day05-alchemical-reduction/main.bash
@@ -15,7 +15,7 @@
 input=$(</dev/stdin)
 sed_pairs="$(echo {a..z} | sed 's/\(\b\w\b\)\s*/\1\u\1|\u\1\1|/g;s/|$//')"
 echo $sed_pairs
-part_1_solution=$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta;s/\s//" | tr -d '\n' | wc -c)
+part_1_solution=$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta;s/\s//g" | tr -d '\n' | wc -c)
 echo "$part_1_solution"
 
 # PART 2
@@ -30,7 +30,7 @@ for char in {a..z}; do
     upperchar=${char^^}
     completely_reacted=${completely_reacted//$upperchar/}
 
-    length=$(echo "$completely_reacted" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)
+    length=$(echo "$completely_reacted" | sed -E ":a s/$sed_pairs//g;ta;s/\s//g" | tr -d '\n' | wc -c)
     if [[ $length -lt $min_length ]]; then
         min_length=$length
     fi
