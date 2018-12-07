a47684b33c8ba1ba630810ddda55baee06b6a9ed
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-07 18:50:40 +0100

    travis test

--- day05-alchemical-reduction/main.bash
+++ day05-alchemical-reduction/main.bash
@@ -10,7 +10,6 @@
 
 input=$(</dev/stdin)
 sed_pairs=$(echo {a..z} | sed 's/\(\b\w\b\)\s*/\1\u\1|\u\1\1|/g;s/|$//')
-echo "sed pairs: $sed_pairs"
 part_1_solution="$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)"
 echo "$part_1_solution"
 
